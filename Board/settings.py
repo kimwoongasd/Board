ed8b44c4238a3d8c1771dfde79a783911bc039d5
som <dndvud123@naver.com>
2021-07-02 20:56:11 +0900

    Bootstrap apply html

--- Board/settings.py
+++ Board/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'Board.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['main/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
